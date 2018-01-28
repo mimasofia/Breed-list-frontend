58e9728dc23f036934babf09ca4b10cccf5ffe6f
mimasofia <virginiadonaire@hotmail.com>
2018-01-28 14:07:46 -0500

    Add div for user messages
    Added a div that says success in sign in or any other auth

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -3,18 +3,18 @@ const store = require('../store')
 
 const signUpSuccess = function (data) {
   // console.log(data)
-  alert('Successfully signed up!')
+  $('.messages').text('Successfully signed up!')
   console.log('succes')
 }
 
 const signUpFailure = function () {
   // console.error(error)
-  alert('Error when signing up. Please try again')
+  $('.messages').text('Error when signing up. Please try again')
   console.log('faliure')
 }
 
 const signInSuccess = function (data) {
-  alert('Successfully signed in!')
+  $('.messages').text('Successfully signed in!')
   console.log('success')
   // want to save this have token and the user d
   // store now has the user stuff after it runs even if the file is emptty
@@ -23,29 +23,29 @@ const signInSuccess = function (data) {
 
 const signInFailure = function () {
   // console.error(error)
-  alert('Error when signing in. Please try again')
+  $('.messages').text('Error when signing in. Please try again')
 }
 
 const changePasswordSuccess = function () {
   // console.log('Successfully changed password')
-  alert('Successfully changed password!')
+  $('.messages').text('Successfully changed password!')
 }
 
 const changePasswordFailure = function (error) {
   console.log(error)
-  alert('Error when changing password. Please try again')
+  $('.messages').text('Error when changing password. Please try again')
 }
 
 const signOutSuccess = function () {
   console.log('Successfully signed out')
-  alert('Successfully signed out!')
+  $('.messages').text('Successfully signed out!')
   store.user = null
 }
 
 const signOutFailure = function (error) {
   console.log(error)
   console.log('fail')
-  alert('Error when signing out. Please try again')
+  $('.messages').text('Error when signing out. Please try again')
 }
 
 module.exports = {

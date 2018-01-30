f0c7acaae5249fe2651df98bd91f4c95da48ed33
mimasofia <virginiadonaire@hotmail.com>
2018-01-30 11:12:48 -0500

    Add hide class to buttons
    Added the hide class to buttons that should be visible when not signed
    in and user messages that explain things in a little more detail

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -15,10 +15,19 @@ const signUpFailure = function () {
 
 const signInSuccess = function (data) {
   $('.messages').text('Successfully signed in!')
-  console.log('success')
+  // console.log('success')
   // want to save this have token and the user d
   // store now has the user stuff after it runs even if the file is emptty
   store.user = data.user
+  $('.details').text('Enjoy making a list of your potential dog-friends!')
+  $('#sign-in-up').addClass('hide')
+  $('#account-pass-out').removeClass('hide')
+  $('#add-friend-here').removeClass('hide')
+  $('#update-friend-here').removeClass('hide')
+  $('#show-full-list').removeClass('hide')
+  $('#show-one-breed').removeClass('hide')
+  $('#delete-one-item').removeClass('hide')
+  $('.list').text('All list related items will appear HERE')
 }
 
 const signInFailure = function () {
@@ -31,22 +40,27 @@ const changePasswordSuccess = function () {
   $('.messages').text('Successfully changed password!')
 }
 
-const changePasswordFailure = function (error) {
-  console.log(error)
+const changePasswordFailure = function () {
+  // console.log(error)
   $('.messages').text('Error when changing password. Please try again')
 }
 
 const signOutSuccess = function () {
   $('.list').empty()
   // console.log('Successfully signed out')
-  $('.messages').text('Successfully signed out!')
+  // $('words-hide').addClass('hide')
   store.user = null
+  $('#sign-in-up').removeClass('hide')
+  $('#account-pass-out').addClass('hide')
+  $('#add-friend-here').addClass('hide')
+  $('#update-friend-here').addClass('hide')
 }
 
-const signOutFailure = function (error) {
-  console.log(error)
-  console.log('fail')
+const signOutFailure = function () {
+  // console.log(error)
+  // console.log('fail')
   $('.messages').text('Error when signing out. Please try again')
+  $('.details').text('Sign-up or Sign-in to start your very own list!')
 }
 
 module.exports = {
--- assets/scripts/breed-list/breedUi.js
+++ assets/scripts/breed-list/breedUi.js
@@ -17,6 +17,7 @@ const createNewListItemSuccess = function (data) {
   $('.list').empty()
   $('.list').append(breedHtml)
   $('.messages').text('Success!')
+  $('.details').hide()
 }
 
 const createNewListItemFailure = function () {
@@ -29,10 +30,12 @@ const showFullListSuccess = function (data) {
   const showBreedHtml = showBreedList({ breed_lists: data.breed_lists })
   $('.list').append(showBreedHtml)
   $('.messages').text('Success! Scroll to see all')
+  $('.details').hide()
 }
 
 const showFullListFailure = function () {
   $('.messages').text('Error try again')
+  $('.details').hide()
 }
 
 const showOneBreedSuccess = function (data) {
@@ -50,11 +53,14 @@ const showOneBreedSuccess = function (data) {
   $('.list').append(breedHtml)
   // $('list').empty()
   $('.messages').text('Success! Scroll to see')
+  $('.details').hide()
 }
 
 const showOneBreedFailure = function () {
   // console.log(error)
-  $('.messages').text('Error getting breed. Please provide valid item ID')
+  $('.messages').text('Error getting breed. Please provide existing dog ID')
+  $('.list').text('=(')
+  $('.details').hide()
 }
 
 const updateListItemSuccess = function (data) {
@@ -70,23 +76,28 @@ const updateListItemSuccess = function (data) {
   )
   $('.list').append(breedHtml)
   $('.messages').text('Success updating. See update below')
+  $('.details').hide()
 }
 
 const updateListItemFailure = function () {
-  $('.messages').text('Error on update. Please Fill out ALL fields')
+  $('.messages').text('Error on update. Please fill out ALL fields and have an existing dog ID')
+  $('.details').hide()
 }
 
 const removeOneListItemSuccess = function () {
   $('.list').empty()
   $('.messages').text('Success removing item from list! Click Show Full list to see new list')
+  $('.details').hide()
 }
 
 const removeOneListItemFailure = function () {
   $('messages').text('Error on removing item from list. Please try again by entering Breed ID')
+  $('.details').hide()
 }
 
 const clearList = () => {
   $('.list').empty()
+  $('.messages').text('')
 }
 
 module.exports = {
